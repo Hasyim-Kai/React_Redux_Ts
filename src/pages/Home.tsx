5981d3662241081fb6129d230ecef0c959d94248
ichihatsu1 <ichihatsu1@gmail.com>
2022-04-09 15:29:07 +0700

    Add Cat Image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import UsersContainer from "../components/UsersContainer";
 
 export default function Home() {
    return <main className="container mx-auto">
+      <img src="/images/cat.jpg" alt="Cat" />
       <h1 className="text-6xl font-bold">Home</h1>
       <HooksCakeContainer />
       <IceCreamContainer />
