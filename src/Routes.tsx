7e603632c1b2b2c80cd943259da42e517aabb269
Kai-69 <ichihatsu1@gmail.com>
2022-04-15 23:08:31 +0700

    Refactor : Add Car Detail Route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
+import CarDetailPage from "./pages/CarDetailPage";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -8,6 +9,7 @@ export default function AppRoutes() {
       <Routes>
          <Route index element={<Auth />} />
          <Route path='home' element={<Home />} />
+         <Route path='car-detail/:id' element={<CarDetailPage />} />
          <Route path='*' element={<NotFound />} />
       </Routes>
    </BrowserRouter >
